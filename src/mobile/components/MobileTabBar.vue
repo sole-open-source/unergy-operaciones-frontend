7674b45bb424365197fae1463a9f4ae675578367
sjacomeUnergy <saraisabel@unergy.io>
2026-07-06 19:36:00 -0500

    fix(mobile): mover Resumen al final de la barra de navegación
    Orden queda: Generación, Fallas, CGM, Resumen.

--- src/mobile/components/MobileTabBar.vue
+++ src/mobile/components/MobileTabBar.vue
@@ -16,11 +16,11 @@
       <RouterLink to="/m/fallas" class="mtb-item" active-class="mtb-item--active">
         <i class="pi pi-wrench" /><span>Fallas</span>
       </RouterLink>
-      <RouterLink to="/m/resumen" class="mtb-item" active-class="mtb-item--active">
-        <i class="pi pi-chart-bar" /><span>Resumen</span>
-      </RouterLink>
       <RouterLink to="/m/reporte-cgm" class="mtb-item" active-class="mtb-item--active">
         <i class="pi pi-envelope" /><span>CGM</span>
+      </RouterLink>
+      <RouterLink to="/m/resumen" class="mtb-item" active-class="mtb-item--active">
+        <i class="pi pi-chart-bar" /><span>Resumen</span>
       </RouterLink>
     </template>
   </nav>
